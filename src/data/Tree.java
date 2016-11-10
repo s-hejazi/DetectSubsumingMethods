a09b4df77a50ddad5d96ff12e823437ac073bf65
samudhraaG <gsamudhraa@gmail.com>
2016-11-09 23:34:09 -0500

    Update Tree.java

--- src/data/Tree.java
+++ src/data/Tree.java
@@ -6,6 +6,23 @@ public class Tree {
 
 	Node root;
 	ArrayList<Method> methods;
+
+	public Node getRoot() {
+		return root;
+	}
+
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+
+	public ArrayList<Method> getMethods() {
+		return methods;
+	}
+
+	public void setMethods(ArrayList<Method> methods) {
+		this.methods = methods;
+	}
+
 	public Tree(Node root, ArrayList<Method> methodList){
 		this.root = root;
 		this.methods = methodList;
