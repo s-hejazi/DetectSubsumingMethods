559908dd26a26d4aa396b45888c3f3f04faee9f8
samudhraaG <gsamudhraa@gmail.com>
2016-11-09 23:38:15 -0500

    Update Main.java

--- src/process/Main.java
+++ src/process/Main.java
@@ -1,6 +1,12 @@
 package process;
 
+import data.Method;
+import data.Node;
+import data.Tree;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -8,14 +14,24 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		CCTreeHandler treeHandler = new CCTreeHandler();
 		File folder = new File("src/profiler");
 		File[] xmlFiles = folder.listFiles();
 		for (File xml : xmlFiles){
 			if(xml.isFile()){
 				System.out.println(xml.getName());
 				XMLParser p = new XMLParser();
-				p.parse(xml);
+				Tree ccTree = p.parse(xml);
+				List<Node> listOfNodes = new ArrayList<>();
+				for(Method treeMethod : ccTree.getMethods()){
+					listOfNodes.addAll(treeMethod.getNodes());
+				}
+				for(Node node : listOfNodes){
+					treeHandler.reduceRecursivePath(node);
+				}
+				for(Method treeMethod : ccTree.getMethods()) {
+					treeHandler.minCPD(treeMethod);
+				}
 				//reduce recursive path
 				//calculate minCPD
 				//calculate height
@@ -27,4 +43,6 @@ public class Main {
 			}
 		}
 	}
+
+
 }
